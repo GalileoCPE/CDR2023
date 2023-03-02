af27af74cd52c9e0db8c078ba4b10184b9638802
Galiléo CPE <galileo@cpe.fr>
2023-03-02 13:13:11 +0100

    Add files via upload

---
+++ BaseRoulante/GALILEO_LIB/2_DC_MOTOR/2_DC_MOTOR.h
@@ -0,0 +1,29 @@
+/*
+ * File: 2_DC_MOTOR.h
+ * Driver Name: [[ 2 DC MOTOR ]]
+ * Created on:
+ * Ver: 1.0
+ * Author:     Galileo CPE
+ *
+ */
+
+#define HAL_TIM_MODULE_ENABLED
+
+// DC Motor Rotation Directions
+#define DIR_FW    0 // forward direction
+#define DIR_BW   1 // backward direction
+
+/*-----[ Prototypes For All Functions ]-----*/
+
+void DC_MOTORS_Init();
+
+void DC_MOTOR_1_Start(uint8_t au8_DIR, uint16_t au16_SPEED);
+void DC_MOTOR_1_Set_Speed(uint16_t au16_SPEED);
+void DC_MOTOR_1_Set_Dir(uint8_t au8_DIR);
+void DC_MOTOR_1_Stop();
+
+void DC_MOTOR_2_Start(uint8_t au8_DIR, uint16_t au16_SPEED);
+void DC_MOTOR_2_Set_Speed(uint16_t au16_SPEED);
+void DC_MOTOR_2_Set_Dir(uint8_t au8_DIR);
+void DC_MOTOR_2_Stop();
+
